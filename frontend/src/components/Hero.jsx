34955098bba79d1a588feb6a2c45637cd3980b8f
ajey108 <ajayxa1@gmail.com>
2024-09-29 22:08:12 +0530

    Add Hero component and add reponsiveness to hero component

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import laptopbanner from '../assets/laptopbanner.png';
+
+const Banner = () => {
+  return (
+    <div className="flex flex-col sm:flex-row items-center p-4 border-4 border-gray-400 bg-gray-100">
+      <div className="flex-1 p-4">
+        <h1 className=" text-2xl font-bold mb-2">Empower Your Productivity</h1>
+        <p className="text-gray-700">
+          Discover the latest in technology with our sleek laptops designed to enhance your workflow and elevate your experience.
+        </p>
+        <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded">
+          Learn More
+        </button>
+      </div>
+      <div className="flex-1">
+        <img
+          className="w-full object-cover"
+          src={laptopbanner}
+          alt="A sleek laptop on a desk showcasing technology"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink,Link } from 'react-router-dom'
 
 import { CiSearch } from "react-icons/ci";
 import { CgProfile } from "react-icons/cg";
 import { HiShoppingBag } from "react-icons/hi2";
+import { IoMenu } from "react-icons/io5";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const Navbar = () => {
+
+  const [visible,setVisible] =useState(false);
   return (
     <div className=' flex  justify-between py-5 font-medium cursor-pointer'>
-      <h1>LapyE-shop</h1>
+      <h1 className='text-white rounded-sm font-bold bg-gray-950 font-serif'>LapyE-shop</h1>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
@@ -56,8 +60,26 @@ const Navbar = () => {
      <HiShoppingBag className='w-[35px]'/>
      <p className='absolute right-[-2px] bottom-[-5px] w-[15px] text-center leading-4 text-black aspect-square rounded-full text-[10px]'>10</p>
      </Link>
+
+     <IoMenu className='sm:hidden' onClick={()=>setVisible(true)} />
       
       </div>
+
+    {/* side bar menu for small screen */}
+
+    <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible?'w-full': 'w-0'}`}>
+    <div className='flex flex-col text-gray-600'>
+                    <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                    
+                    <IoMdArrowRoundBack />
+                        <p>Back</p>
+                    </div>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                    <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+                </div>
+    </div>
 
     </div>
   )
