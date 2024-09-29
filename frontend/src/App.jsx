2868d9aaf95e8735be2802b86f4f0fb6b5848327
ajey108 <ajayxa1@gmail.com>
2024-09-29 11:21:43 +0530

    Add Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Collections from './pages/Collections';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Orders from './pages/Orders';
+import PlaceOrder from './pages/PlaceOrder';
+import Product from './pages/Product';
+import Navbar from './components/Navbar';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:pw=[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+
+    <Route path= '/' element={<Home/>} />
+    <Route path= '/collection' element={<Collections/>} />
+    <Route path= '/about' element={<About/>} />
+    <Route path= '/contact' element={<Contact/>} />
+    <Route path= '/product/:productId' element={<Product/>} />
+    <Route path= '/cart' element={<Cart/>} />
+    <Route path= '/login' element={<Login/>} />
+    <Route path= '/place-order' element={<PlaceOrder/>} />
+    <Route path= '/orders' element={<Orders/>} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { NavLink,Link } from 'react-router-dom'
+
+import { CiSearch } from "react-icons/ci";
+import { CgProfile } from "react-icons/cg";
+import { HiShoppingBag } from "react-icons/hi2";
+
+const Navbar = () => {
+  return (
+    <div className=' flex  justify-between py-5 font-medium cursor-pointer'>
+      <h1>LapyE-shop</h1>
+
+      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+
+      <NavLink to='/' className='flex flex-col items-center gap-1'>
+        <p>Home</p>
+        <hr className='w-2/4 border-none h-[2px] bg-gray-700 hidden' />
+      </NavLink>
+
+      <NavLink to='/about' className='flex flex-col items-center gap-1'>
+        <p>About</p>
+        <hr className='w-2/4 border-none h-[2px] bg-gray-700 hidden' />
+      </NavLink>
+
+      <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+        <p>Collection</p>
+        <hr className='w-2/4 border-none h-[2px] bg-gray-700 hidden' />
+      </NavLink>
+
+      <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+        <p>Contact</p>
+        <hr className='w-2/4 border-none h-[2px] bg-gray-700 hidden' />
+      </NavLink>
+
+
+      </ul>
+
+      <div className="flex items-center gap-5">
+      <CiSearch/>
+
+
+      <div className='group relative'>
+      <CgProfile />
+
+      <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+        <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text">
+         <p className='hover:text-black'>MyProfile</p>
+         <p className='hover:text-black'>Orders</p>
+         <p className='hover:text-bg-red-400'>Logout</p>
+        </div>
+
+      </div>
+      </div>
+
+     <Link to='/cart ' className='relative'>
+     <HiShoppingBag className='w-[35px]'/>
+     <p className='absolute right-[-2px] bottom-[-5px] w-[15px] text-center leading-4 text-black aspect-square rounded-full text-[10px]'>10</p>
+     </Link>
+      
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
