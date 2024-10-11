32dfc9dfeeb61c6a62475b6ae4c21aee05547e8c
ajey108 <ajayxa1@gmail.com>
2024-10-11 09:14:36 +0530

    Create product schema for MongoDB and integrate Cloudinary for image uploads

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+
+//app config
+
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//api endpoints
+
+app.get('/', (req, res) => {
+    res.status(200).send('app is running');
+})      
+
+app.listen(port, () => console.log(`server is running on port ${port}`))
