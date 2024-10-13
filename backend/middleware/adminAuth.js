b77aa013fd864432cc19260d42f776da0ab22367
ajey108 <ajayxa1@gmail.com>
2024-10-13 14:36:51 +0530

    Add middleware  for admin authorization

---
+++ backend/middleware/adminAuth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+
+
+const adminAuth = async (req, res, next) => {
+    try {
+        const {token} =req.headers;
+        if(!token){
+            return res.json({success:false,message:"Unauthorized"})
+        }
+
+        const token_decode = jwt.verify(token,process.env.JWT_SECRET)
+        if(token_decode !== process.env.ADMIN_EMAIL+process.env.ADMIN_PASSWORD){
+            return res.json({success:false,message:"Unauthorized"})
+        }
+
+        next()
+
+    } catch(error){
+        console.log(error)
+        return res.status(401).json({success:false,message:"Unauthorized"})
+    }
+
+}
+
+export default adminAuth;
+             
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { addProduct, listProducts, removeProduct, singleProduct } from '../controllers/productController.js';
 import upload from '../middleware/multer.js';
+import adminAuth from '../middleware/adminAuth.js';
 
 const productRouter = express.Router();
 
 //api endpoints
-productRouter.post('/add', upload.single('image'), addProduct);
+productRouter.post('/add',adminAuth, upload.single('image'), addProduct);
 productRouter.get('/list',listProducts);
-productRouter.delete('/remove',removeProduct);
+productRouter.delete('/remove',adminAuth,removeProduct);
 productRouter.get('/single',singleProduct);
 
 export default productRouter;
