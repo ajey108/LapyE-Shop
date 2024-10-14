c0bf111a2ba47e3f8049f38949c058d5e548f33a
ajey108 <ajayxa1@gmail.com>
2024-10-14 11:58:25 +0530

    Add login functionality in admin , connect backend adminLogin url with the frontend, add React toastify.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,31 +1,45 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Add from './pages/Add'
 import List from './pages/List';
 import Orders from './pages/Orders';
+import { useState } from 'react';
+import Login from './components/Login';
+
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
 
-  const [token,setToken] = useState('');
+  const [token, setToken] = useState('');
+
   return (
-   <div className='bg-gray-800 text-white min-h-screen'>
-   <>
-  
-   <Navbar/>
-   <hr />
-   <div className="flex w-full">
-    <Sidebar/>
-    <div className="w-[70%] mx-auto ml-[max(5vw,25px) my-8 text-gray-600 text-base]"></div>
-    <Routes>
-      <Route path='/add' element={<Add/>}/>
-      <Route path='/add' element={<List/>}/>
-      <Route path='/add' element={<Orders/>}/>
-    </Routes>
-   </div>
-   </>
-   </div>
+    <div className='bg-gray-800 text-white min-h-screen'>
+
+      <ToastContainer />
+      {
+        token === '' ? <Login setToken={setToken} /> :
+          <>
+
+            <Navbar />
+            <hr />
+            <div className="flex w-full">
+              <Sidebar />
+              <div className="w-[70%] mx-auto ml-[max(5vw,25px) my-8 text-gray-600 text-base]"></div>
+              <Routes>
+                <Route path='/add' element={<Add />} />
+                <Route path='/add' element={<List />} />
+                <Route path='/add' element={<Orders />} />
+              </Routes>
+            </div>
+          </>
+      }
+
+    </div>
   )
 }
 
--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { backendUrl } from '../App';
 
-const Login = () => {
-  return (
-    <div className=""></div>
-  )
-}
+const Login = ({setToken}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-export default Login
+    // Submit handler function
+    const submitHandler = async (e) => {
+        e.preventDefault();  
+
+        try {
+            const response = await axios.post(`${backendUrl}/api/user/admin`, { email, password });
+          
+            if (response.data.success) {
+               setToken(response.data.token);
+            } else{
+                toast.error(response.data.message);
+            }
+        } catch (error) {
+            console.error(error);
+            toast.error(error.message);
+           
+        }
+    };
+
+    return (
+        <div className="flex items-center justify-center mb-4 cursor-pointer min-h-[80vh] min-w-[80vw]">
+            <div className="border top-8 text-black p-4">
+                <h1>Admin Login</h1>
+                <form onSubmit={submitHandler} className='mt-9'>
+                    <div className="mb-4">
+                        <p>Email</p>
+                        <input
+                            onChange={(e) => setEmail(e.target.value)}
+                            className='rounded-lg'
+                            value={email}
+                            type="email"
+                            placeholder='your email'
+                            autoComplete='email'
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <p>Password</p>
+                        <input
+                            onChange={(e) => setPassword(e.target.value)}
+                            className='rounded-lg'
+                            value={password}
+                            type="password"
+                            placeholder='your password'
+                            autoComplete='current-password'  // Changed to 'current-password'
+                            required
+                        />
+                    </div>
+
+                    <button className='bg-black text-white py-2 px-4 rounded-lg'>
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
