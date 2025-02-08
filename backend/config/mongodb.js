95addee1028daeffd801b8b4ef7687daffb00b01
Ajay <ajayxa1@gmail.com>
2025-02-08 11:33:12 +0530

    update env

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    mongoose.connection.on('connected',()=>{
-       console.log('DB Connected') 
-    })
-   await mongoose.connect(`${process.env.MONGODB_URI}`)
+  mongoose.connection.on("connected", () => {
+    console.log("DB Connected");
+  });
+  await mongoose.connect(`${process.env.MONGODB_URI}`);
 };
 
 export default connectDB;
